3f56adfda5f691185eeb7ff4db0dd80f2834ce32
Michael J. Lee <mikaly@gmail.com>
2017-03-28 15:48:56 -0400

    Show examples for control flow and conditionals

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
 <?php
-phpinfo();
+
+$num = 5;
+$message = "This is a message";
+
+$name = "Mike";
+
+
+/*
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+} 
+*/
+
+$fname = "Mike";
+$lname = "Lee";
+	
+
+if ($fname == "Mike" && $fname == "Lee"){
+	echo "Result 1";
+}
+else {
+	echo "Result 2";
+}
+
+/*
+while ($string != "Two"){
+	echo $string;
+	$string = "Two";
+}
+*/
+
+//echo $message;
+//echo $_GET['fname'];
+//echo " ";
+//echo $_GET['lname'];
+
+/*
+if ($_GET['num'] > 2){
+	echo "Greater than 2!";
+	
+	if ($name == "Mike") {
+		echo " Hi Mike";
+	}
+	
+}
+else if ($_GET['num'] == 2){
+	echo "Equal to 2!";
+}
+else {
+	echo "Less than 2!";
+}
+*/
+
+//$lname = "Lee";
+//$fname = "Mike $lname ";
+
+
+
+
+
 ?>
