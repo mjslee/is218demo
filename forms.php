1a8da7cb6c45f1c8273ed5a0fb33340b2e59c1f8
Michael J. Lee <mikaly@gmail.com>
2017-04-04 14:26:19 -0400

    Include forms example

---
+++ forms.php
@@ -0,0 +1,14 @@
+<html>
+	<body>
+	
+	<form action="form-handler.php" method="get">
+		Name: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		Password: <input type="password" name="password"><br>
+		<br>
+		<textarea>test</textarea>
+		<input type="submit">
+	</form>
+	
+	</body>
+</html>
